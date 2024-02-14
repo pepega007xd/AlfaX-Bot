a5e40ea178537cbaa35db1d32de2fe37f4aadbba
Jastrobaron <kubo.hresko@gmail.com>
2024-02-14 19:14:11 +0100

    Prolonged timeout for OpenAI API calls.

--- src/main/java/xyz/rtsvk/alfax/commands/implementations/ChatGPTCommand.java
+++ src/main/java/xyz/rtsvk/alfax/commands/implementations/ChatGPTCommand.java
@@ -1,32 +1,30 @@
 package xyz.rtsvk.alfax.commands.implementations;
 
+import com.theokanning.openai.completion.chat.ChatCompletionChoice;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.theokanning.openai.service.OpenAiService;
 import discord4j.common.util.Snowflake;
 import discord4j.core.GatewayDiscordClient;
-import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.User;
 import discord4j.core.object.entity.channel.MessageChannel;
 import xyz.rtsvk.alfax.commands.Command;
 import xyz.rtsvk.alfax.util.Config;
-import xyz.rtsvk.alfax.util.Logger;
 
+import java.time.Duration;
 import java.util.List;
 
 public class ChatGPTCommand implements Command {
 
-	private final Logger logger;
 	private final Config config;
 
 	public ChatGPTCommand(Config config) {
-		this.logger = new Logger(this.getClass());
 		this.config = config;
 	}
 
 	@Override
-	public void handle(User user, MessageChannel channel, List<String> args, Snowflake guildId, GatewayDiscordClient bot) throws Exception {
+	public void handle(User user, Snowflake messageId, MessageChannel channel, List<String> args, Snowflake guildId, GatewayDiscordClient bot) throws Exception {
 		if (this.config.getBoolean("openai-disabled")) {
 			channel.createMessage("Tento prikaz je zakazany administratorom!").block();
 			return;
@@ -44,27 +42,23 @@ public class ChatGPTCommand implements Command {
 			return;
 		}
 
-		Message msg = channel.createMessage("Hmmm... :thinking:\n").block();
-		if (msg == null) {
-			this.logger.error("Failed to send message!");
-			return;
-		}
-
 		StringBuilder output = new StringBuilder();
-		OpenAiService service = new OpenAiService(this.config.getString("openai-api-key"));
+		OpenAiService service = new OpenAiService(
+				this.config.getString("openai-api-key"),
+				Duration.ofMinutes(1));
 		ChatCompletionRequest completionRequest = ChatCompletionRequest.builder()
 				.messages(List.of(new ChatMessage(ChatMessageRole.USER.value(), messageContent)))
 				.model(this.config.getString("openai-model"))
 				.build();
-		service.createChatCompletion(completionRequest).getChoices()
-				.forEach(e -> {
-					String text = e.getMessage().getContent();
-					output.append(text);
-				});
+		List<ChatCompletionChoice> choices = service.createChatCompletion(completionRequest).getChoices();
+		choices.forEach(e -> {
+			String text = e.getMessage().getContent();
+			output.append(text);
+		});
 
 		String response = output.toString()
 				.replace("@", "@ ");
-		msg.edit(spec -> spec.setContent(response)).block();
+		channel.createMessage(response).withMessageReference(messageId).block();
 	}
 
 	@Override
