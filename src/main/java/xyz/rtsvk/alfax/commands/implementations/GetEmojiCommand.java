eedb84981eba3196917d3aa83d13d4f259d0ea0c
Jakub Hreško <91632574+Jastrobaron@users.noreply.github.com>
2024-12-26 11:43:08 +0100

    Fix `getemoji` command (#12)

--- src/main/java/xyz/rtsvk/alfax/commands/implementations/GetEmojiCommand.java
+++ src/main/java/xyz/rtsvk/alfax/commands/implementations/GetEmojiCommand.java
@@ -1,41 +1,39 @@
 package xyz.rtsvk.alfax.commands.implementations;
 
-import discord4j.common.util.Snowflake;
 import discord4j.core.GatewayDiscordClient;
-import discord4j.core.object.entity.Guild;
-import discord4j.core.object.entity.GuildEmoji;
 import discord4j.core.object.entity.User;
 import xyz.rtsvk.alfax.util.guildstate.GuildState;
 import xyz.rtsvk.alfax.commands.ICommand;
 import xyz.rtsvk.alfax.util.chatcontext.IChatContext;
 import xyz.rtsvk.alfax.util.text.MessageManager;
+import xyz.rtsvk.alfax.util.text.TextUtils;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GetEmojiCommand implements ICommand {
 
+	private static final Pattern EMOJI_STRING_REGEX = Pattern.compile("<:[a-zA-z]+:[0-9]+>");
+	private static final Pattern EMOJI_ID_REGEX = Pattern.compile(":(\\d+)>");
+
 	@Override
 	public void handle(User user, IChatContext chat, List<String> args, GuildState guildState, GatewayDiscordClient bot, MessageManager language) throws Exception {
-		Guild server = bot.getGuildById(guildState.getGuildId()).block();
-		if (server == null) {
-			chat.sendMessage(language.getMessage("command.get-emoji.error.server-not-found"));
-			return;
-		}
-
 		if (args.isEmpty()) {
 			chat.sendMessage(language.getFormattedString("command.usage").addParam("usage", this.getUsage()).addParam("prefix", chat.getCommandPrefix()).build());
 			return;
 		}
 
 		String arg = args.get(0);
-		String emoji;
-		if (arg.startsWith("<:") && arg.endsWith(">")) {
-			emoji = arg.substring(2, arg.length() - 1);
-			server.getEmojis()
-					.filter(e -> e.getId().equals(Snowflake.of(emoji.substring(emoji.indexOf(":") + 1))))
-					.map(GuildEmoji::getImageUrl)
-					.doOnNext(chat::sendMessage)
-					.doOnError(e -> chat.sendMessage(language.getFormattedString("command.get-emoji.error").addParam("error", e.getMessage()).build()));
+		if (!EMOJI_STRING_REGEX.matcher(arg).matches()) {
+			chat.sendMessage(language.getMessage("command.get-emoji.error.invalid-emoji-format"));
+			return;
+		}
+
+		Matcher idMatcher = EMOJI_ID_REGEX.matcher(arg);
+		if (idMatcher.find()) {
+			String emojiID = idMatcher.group(1);
+			chat.sendMessage(TextUtils.format("https://cdn.discordapp.com/emojis/${0}.png", emojiID));
 		} else {
 			chat.sendMessage(language.getMessage("command.get-emoji.error.invalid-emoji-format"));
 		}
